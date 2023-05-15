da80f36e75b42557fe6f35b1044ec46d5f7719fc
Diego Borigen <diego@iddigital.com.au>
2023-05-15 23:03:50 +1000

    Fix menu overflow

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={`${styles.main}`}>
+            <main className={`${styles.main} relative overflow-hidden`}>
                 <Header />
                 <Banner />
                 <PickYourTrip />
