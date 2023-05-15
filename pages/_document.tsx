6372e4de77f5440dd32ed8f251206fb8c82563b8
Diego Borigen <diego@iddigital.com.au>
2023-05-16 08:34:03 +1000

    Install google Analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta property='og:type' content='website' />
+                    <meta property='og:title' content='The Hipposoft' />
+                    <meta property='og:description' content="We create to connect." />
+                    <meta property='og:site_name' content='The Hipposoft' />
+
+                    <script type="application/ld+json"
+                        dangerouslySetInnerHTML={{
+                            __html: `{
+                                "@context": "https://schema.org",
+                                "@type": "Organization",
+                                "name": "Hipposoft",
+                                "url": "https://www.thehipposoft.com/",
+                                "logo": "https://www.thehipposoft.com/assets/logo.png",
+                                "contactPoint": {
+                                    "@type": "ContactPoint",
+                                    "telephone": "0414286242",
+                                    "contactType": "customer service",
+                                    "areaServed": "DE",
+                                    "availableLanguage": "de"
+                                },
+                                "sameAs": []
+                            }`,
+                        }}
+                    />
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=G-8JFFD0YM8L"
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-8JFFD0YM8L', {
+                                  page_path: window.location.pathname,
+                                });
+                              `,
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+    
+}
