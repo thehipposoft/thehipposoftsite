9ee33014ed5df6c068d0d843d37b8e230157beed
Diego Borigen <diego@iddigital.com.au>
2023-06-22 07:43:00 +1000

    Fix website

---
+++ pages/blog.tsx
@@ -0,0 +1,42 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+
+export default function Blog( { blogs } ) {
+    
+    return (
+        <div className={'flex flex-col'}>
+            <h1 className={'text-center text-black w-full'}>Blogs</h1>
+            <div className={'w-full bg-slate-200 flex'}>
+                {
+                    blogs.map((item, index) => <div>
+                        <h3>{item.title}</h3>
+                    </div>)
+                }
+            </div>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    // List of files in blogs folder
+    const filesInBlogs = fs.readdirSync('./content/blogs')
+  
+    // Get the front matter and slug (the filename without .md) of all files
+    const blogs = filesInBlogs.map(filename => {
+      const file = fs.readFileSync(`./content/blogs/${filename}`, 'utf8')
+      const matterData = matter(file)
+  
+      return {
+        ...matterData.data, // matterData.data contains front matter
+        slug: filename.slice(0, filename.indexOf('.'))
+      }
+    })
+
+    console.log('>>blogs', blogs);
+  
+    return {
+      props: {
+        blogs
+      }
+    }
+}
