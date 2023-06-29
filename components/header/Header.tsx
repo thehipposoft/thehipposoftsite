de6ba898cbc56e185ab45cd5973f2562047fdeb6
Diego Borigen <diego@iddigital.com.au>
2023-06-29 22:00:58 +1000

    Text wink effect

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
 import { motion } from 'framer-motion';
+import { CSSTransition } from 'react-transition-group';
 
 //Components
 import Button from '../common/Button';
@@ -9,32 +9,98 @@ import styles from './Header.module.scss';
 import SideMenu from '../sideMenu';
 import BackDrop from '../sideMenu/backDrop';
 
-const Header = () => {
-    const [sideMenu, setSideMenu] = useState(false);
+const imageUrls = [
+  '/assets/logo.png',
+  '/assets/images/home/hippo2.png',
+  '/assets/images/home/hippo3.png',
+  '/assets/images/home/hippo4.png',
+  '/assets/images/home/hippo5.png',
+];
 
-    const toggleSideMenu = () => {
-        setSideMenu((prevState) => !prevState)
+const Header = () => {
+  const [sideMenu, setSideMenu] = useState(false);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const startImageTransition = () => {
+    setIsPlaying(true);
+  };
+
+  const stopImageTransition = () => {
+    setIsPlaying(false);
+    setCurrentImageIndex(0);
+  };
+
+  useEffect(() => {
+    let interval;
+
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+      }, 100);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isPlaying, imageUrls.length]);
+
+  useEffect(() => {
+    const preloadImages = async () => {
+      const promises = imageUrls.map((url) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.onload = resolve;
+          img.onerror = reject;
+          img.src = url; // Move the assignment here
+        });
+      });
+
+      try {
+        await Promise.all(promises);
+        setImagesLoaded(true);
+      } catch (error) {
+        console.error('Failed to preload images:', error);
+      }
     };
 
-    return (
-        <motion.div 
-            className={styles.container}
-            initial={{opacity: 0}}
-            whileInView={{opacity: 1}}
-            transition={{duration: 1.5}}
-            viewport={{ once: true }}
+    preloadImages();
+  }, [imageUrls]);
+
+  const toggleSideMenu = () => {
+    setSideMenu((prevState) => !prevState);
+  };
+
+  return (
+    <motion.div
+      className={styles.container}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
+    >
+      {imagesLoaded && (
+        <CSSTransition
+          key={currentImageIndex}
+          timeout={200}
+          classNames="image-transition"
         >
-            <Image
-                src={'/assets/logo.png'}
-                alt={'Hipposoft Logo'} 
-                width={154} 
-                height={60} 
-            />
-            <Button label={'Menu'} onClick={toggleSideMenu} />
-            <SideMenu sideMenu={sideMenu} closeSideMenu={toggleSideMenu}/>
-            <BackDrop sideMenu={sideMenu} closeSideMenu={toggleSideMenu}/>
-        </motion.div>
-    )
+          <img
+            className={'w-[164px]'}
+            src={imageUrls[currentImageIndex]}
+            alt="Current Image"
+            onMouseEnter={startImageTransition}
+            onMouseLeave={stopImageTransition}
+          />
+        </CSSTransition>
+      )}
+
+      <Button label={'Menu'} onClick={toggleSideMenu} />
+      <SideMenu sideMenu={sideMenu} closeSideMenu={toggleSideMenu} />
+      <BackDrop sideMenu={sideMenu} closeSideMenu={toggleSideMenu} />
+    </motion.div>
+  );
 };
 
 export default Header;
